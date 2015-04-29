eb22a7659be14933fd1c1f5b0722de286a3455ce
Diurno <Diurno@C7PC17>
2015-04-29 09:18:31 +0200

    Mi primer commit by Miguel Cenzano

---
+++ src/MiguelCenzano/ClaseMiguelCenzano.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package MiguelCenzano;
+
+/**
+ *
+ * @author Miguel Cenzano
+ */
+public class ClaseMiguelCenzano {
+    private String mensaje;
+    
+    public ClaseMiguelCenzano()
+    {
+       mensaje = "Hola mundo"; 
+    
+    }
+    public String getMensaje()
+    {
+        return mensaje;
+    }
+}
