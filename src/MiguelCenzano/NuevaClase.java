25f0738037edfea43cf03ab3fba7630edbf51191
Diurno <Diurno@C7PC17>
2015-05-06 08:52:16 +0200

    Mi primer commit de la nueva clase creada

---
+++ src/MiguelCenzano/NuevaClase.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package MiguelCenzano;
+
+/**
+ *
+ * @author Diurno
+ */
+public class NuevaClase {
+    private int numero;
+    private String cadena;
+public NuevaClase(int numVeces)
+{
+    numero = numVeces;
+    cadena = "Hola";
+}  
+public String cdaenaRepetida()
+{
+    return cadena;
+}
+}
