d032f97da490c4dc3477e15c6044428d533e31bf
Diurno <Diurno@C7PC15>
2015-05-06 09:04:19 +0200

    Mi tercer commit

---
+++ src/paqueteRocio/ModificadaPorAmbos.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package paqueteRocio;
+
+/**
+ *
+ * @author Diurno
+ */
+public class ModificadaPorAmbos {
+    private int x;
+   private int y;
+   
+   public ModificadaPorAmbos()
+   {
+       x=2;
+       y=3;
+   }
+   
+   public void sumar()
+   {
+       int suma = x+y;
+       System.out.println(suma);
+      
+   }
+}
